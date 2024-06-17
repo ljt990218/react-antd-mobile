f7a5a2ed2c4420fa88a4222b879b646775fa7302
ljt990218 <2511590093@qq.com>
2024-06-17 17:58:53 +0800

    refactor(hooks): remove useInterval and usePageLoading hooks

--- src/hooks/usePageLoading.ts
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-import { endLoading, startLoading } from '@/utils/nprogress';
-
-export function usePageLoading() {
-	useEffect(() => {
-		startLoading();
-		return () => endLoading();
-	}, []);
-}
