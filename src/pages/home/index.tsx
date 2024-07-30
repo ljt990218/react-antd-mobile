82b63c389c61f880ae3f3c5e211d396364b739e5
ljt990218 <2511590093@qq.com>
2024-07-30 15:24:56 +0800

    fix: Improve the quality of international support

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import { memo, useLayoutEffect, useState } from 'react'
+import { memo } from 'react'
 import { Button, Space, Switch } from 'antd-mobile'
 import { useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
@@ -18,12 +18,12 @@ const HomePage = memo(() => {
   }
 
   const useGlobal = useGlobalStore() as any
-  console.log(useGlobal)
 
   return (
     <div className="relative">
       <select
         className="absolute right-10 top-10"
+        value={i18n.language}
         onChange={(evt) => {
           i18n.changeLanguage(evt.target.value)
         }}
@@ -33,11 +33,11 @@ const HomePage = memo(() => {
         ))}
       </select>
 
-      <h1 className="text-32px bg-gradient-to-r from-blue-600 to-pink-600 bg-clip-text text-center font-bold text-transparent">{t('leftMenus.home')}</h1>
+      <h1 className="text-32px bg-gradient-to-r from-blue-600 to-pink-600 bg-clip-text text-center font-bold text-transparent">{t('home')}</h1>
 
       <div className="mt-10 flex flex-col items-center">
         <Space align="center">
-          <div>Dark Mode</div>
+          <div>{t('darkMode')}</div>
           <Switch
             checked={useGlobal.isDark}
             onChange={(v) => {
