314bb445829b34d813ef81c322da2e18bc416252
ljt990218 <2511590093@qq.com>
2024-07-31 23:49:44 +0800

    chore: del `postcss-mobile-forever`

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import type { ConfigEnv } from 'vite';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import Unocss from 'unocss/vite';
-import viewport from 'postcss-mobile-forever'
+// import viewport from 'postcss-mobile-forever'
 import autoprefixer from 'autoprefixer'
 
 // Icons 自动按需引入图标库
@@ -33,11 +33,11 @@ export default defineConfig(({ mode }: ConfigEnv) => {
 				plugins: [
 					autoprefixer(),
 					// https://github.com/wswmsword/postcss-mobile-forever
-					viewport({
-						appSelector: '#root',
-						viewportWidth: 375,
-						maxDisplayWidth: 600,
-					}),
+					// viewport({
+					// 	appSelector: '#root',
+					// 	viewportWidth: 375,
+					// 	maxDisplayWidth: 600,
+					// }),
 				]
 			}
 		},
